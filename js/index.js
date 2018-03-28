76504155d93137e08a326aac7be5239552d23d6e
青柳 恭義 <takayoshi_aoyagi@waku-2.com>
2018-03-28 16:52:02 +0900

    Initial revision

---
+++ js/index.js
@@ -0,0 +1,25 @@
+(function () {
+
+    "use strict";
+
+    $('.navbar-nav>li>a').on('click', function(){
+	$('.navbar-collapse').collapse('hide');
+    });
+
+    
+    const app = {};
+    const array = [];
+    app.gc = new GoogleCalendar();
+    app.gc.getEvents(function (err, events) {
+	if (err || !events) {
+	    console.log(err);
+	    return;
+	}
+	events.forEach(function (event) {
+	    console.log(event);
+	    array.push(new app.Schedule(event));
+	});
+	//app.schedules.reset(array);
+    });
+
+})();
